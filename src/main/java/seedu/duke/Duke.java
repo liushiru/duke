b3a64a1ace99361477e4a74e9543c058ac399a6b
liushiru <shiruliu55@hotmail.com>
2019-08-24 22:14:46 +0800

    Level 3. Mark as Done (class)

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -1,6 +1,7 @@
 package seedu.duke;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Duke {
     public static void main(String[] args) {
@@ -16,15 +17,27 @@ public class Duke {
         Scanner input = new Scanner(System.in);
         String userInput = input.nextLine();
 
-        ArrayList<String> inputArray = new ArrayList<String>();
+        ArrayList<Task> inputArray = new ArrayList<Task>();
         while (!userInput.equals("bye")) {
+
+            String[] tokens = userInput.split(" ");
+
+
             if (userInput.equals("list")) {
                 for(int i = 0; i < inputArray.size(); i++) {
-                    System.out.println((i + 1) + ". " + inputArray.get(i));
+                    String description = inputArray.get(i).getDescription();
+                    System.out.println((i + 1) + ".[" + inputArray.get(i).getStatusIcon() + "] " + description);
                 }
                 userInput = input.nextLine();
+            } else if (tokens[0].equals("done")) {
+                int taskNum = Integer.parseInt(tokens[1]) - 1;
+                inputArray.get(taskNum).setDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("[" + inputArray.get(taskNum).getStatusIcon() + "] " + inputArray.get(taskNum).getDescription());
+                userInput = input.nextLine();
             } else {
-                inputArray.add(userInput);
+                Task newTask= new Task(userInput);
+                inputArray.add(newTask);
                 System.out.println("added: " + userInput);
                 userInput = input.nextLine();
             }
---
+++ src/main/java/seedu/duke/Task.java
@@ -0,0 +1,29 @@
+package seedu.duke;
+
+import java.util.ArrayList;
+
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public boolean getStatus() {
+        return this.isDone;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+    }
+
+    public void setDone() {
+        this.isDone = true;
+    }
+}
