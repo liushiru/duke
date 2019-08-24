dba82488b9acb8a44a2f6f041ea5aa507754e95d
liushiru <shiruliu55@hotmail.com>
2019-08-24 20:01:57 +0800

    Level-1 Greet, Echo, Exit

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -1,3 +1,6 @@
+package seedu.duke;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +9,15 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("");
+        System.out.println("");
+        Scanner input = new Scanner(System.in);
+
+        String userInput = input.next();
+        while (!userInput.equals("bye")) {
+            System.out.println(userInput);
+            userInput = input.nextLine();
+        }
     }
+
 }
---
+++ src/test/java/seedu/duke/DukeTest.java
@@ -0,0 +1,7 @@
+package seedu.duke;
+
+class DukeTest {
+    public static void main(String args[]){
+        System.out.println("Hello Java");
+    }
+}
